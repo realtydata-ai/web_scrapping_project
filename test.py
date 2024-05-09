751330ef28da062a947547e3afb958f3814e4130
agustinperalta <aguspm.am@gmail.com>
2024-05-09 02:19:56 -0300

    proxy_test

--- test.py
+++ test.py
@@ -5,52 +5,77 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium_stealth import stealth
 from flask import Flask  # Importa la clase Flask del módulo flask
-from pyvirtualdisplay import Display
 import undetected_chromedriver as uc
 
 app = Flask(__name__)  # Crea una instancia de la aplicación Flask
 
 def process_page():
-    # Configura el proxy
-    proxy = '200.32.51.179:8080'
-    proxy_options = {
-        'proxy': {
-            'http': proxy,
-            'https': proxy,
-            'no_proxy': '',
-            'proxyType': 'MANUAL',
-        }
-    }
+    user_agents = [
+    # Add your list of user agents here
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+]
+    
+    ips = ['45.238.220.1:8181','200.61.16.80:8080']
+    
+
 
     # Configura las opciones del navegador Chrome WebDriver
     options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')
-    options.add_argument('start-maximized')
-    options.add_argument('enable-automation')
-    options.add_argument('--disable-infobars')
+    #run in headless mode
     options.add_argument("--headless")
+
+    # disable the AutomationControlled feature of Blink rendering engine
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    # disable pop-up blocking
+    options.add_argument('--disable-popup-blocking')
+
+    # start the browser window in maximized mode
+    options.add_argument('--start-maximized')
+
+    # disable extensions
+    options.add_argument('--disable-extensions')
+
+    # disable sandbox mode
+    options.add_argument('--no-sandbox')
+
+    # disable shared memory usage
     options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-browser-side-navigation')
-    #options.add_argument("--remote-debugging-port=9222")
-    #options.add_argument('--disable-gpu')
+
+    #
     options.add_argument("--log-level=3")
 
+    user_agent = random.choice(user_agents)
+
+    #options.add_argument(f'user-agent={user_agent}')
+
+    proxy = random.choice(ips)
     # Agrega las opciones del proxy al navegador
-    options.add_experimental_option('prefs', proxy_options)
+    options.add_argument(f'--proxy-server={proxy}')
+
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
+    options.add_experimental_option("useAutomationExtension",False)
+    #options.page_load_strategy = 'normal'
 
     browser = webdriver.Chrome(options=options)
-    # stealth(browser,
-    #         languages=["es"],
-    #         vendor="Debian",
-    #         platform="Linux x86_64",
-    #         webgl_vendor="AMD",
-    #         renderer="AMD Inc."
-    #         )
-
+    stealth(browser,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
     url = "https://www.zonaprop.com.ar/inmuebles-alquiler-cordoba.html"
     browser.get(url)
 
-    time.sleep(random.randint(4, 8))
+    time.sleep(random.randint(20, 30))
 
     html = browser.page_source
     print(html)
