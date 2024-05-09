8c3c084f45b23b76ad2b4dcec086d56abcd9de31
agustinperalta <aguspm.am@gmail.com>
2024-05-09 03:27:55 -0300

    152.231.62.1 proxy add

--- functions_scrapping.py
+++ functions_scrapping.py
@@ -282,7 +282,7 @@ def process_property(to_post):
     :param to_post: La URL de la propiedad que se va a procesar.
     :return: Un diccionario que contiene la información procesada de la propiedad.
     """
-    ips = ['181.209.125.186:999']
+    ips = ['152.231.62.1:999']
     # Configura las opciones del navegador Chrome WebDriver
     options = webdriver.ChromeOptions()
     #run in headless mode
@@ -380,7 +380,7 @@ def process_page(url):
     :param url: La URL de la página que se va a procesar.
     :return: El resultado del procesamiento de la página.
     """
-    ips = ['181.209.125.186:999']
+    ips = ['152.231.62.1:999']
     # Configura las opciones del navegador Chrome WebDriver
     options = webdriver.ChromeOptions()
     #run in headless mode
